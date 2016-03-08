a8786419d86e90f471e9c626b136a71abe9a0687
iversongit <1564329410@qq.com>
2016-03-08 02:18:59 -0800

    homework 20160305

--- MyString.cpp
+++ MyString.cpp
@@ -10,13 +10,56 @@ using std::cout;
 using std::endl;
 
 class String{
+	private:
+		class CharProxy{
+			public:
+				CharProxy(String &theString,int index)
+				:_theString(theString)
+				,_index(index)
+				{
+					cout<<"CharProxy()"<<endl;
+				}
+
+				char & operator=(const char& ch){
+					cout<<"CharProxy::operator=(const char&)"<<endl;
+					if(_index>=0 && _index<_theString.size()){
+						if(_theString._str[_theString.size() + 1]==1){
+							_theString._str[_index] = ch;
+							return _theString._str[_index];
+						}else{
+							--_theString._str[_theString.size() + 1];
+							char *temp = _theString._str;
+							_theString._str = new char[_theString.size() + 2];
+							strcpy(_theString._str,temp);
+							_theString._str[_theString.size() + 1] = 1;
+							_theString._str[_index] = ch;
+							return _theString._str[_index];
+						}
+					}else{
+						static char nullchar = '\0';
+						cout<<"下标越界"<<endl;
+						return nullchar;
+					}				
+
+				}
+
+				operator char(){
+					cout<<"CharProxy::operator char()"<<endl;
+					return _theString._str[_index];
+				}
+			private:
+				String & _theString;
+				int _index;
+
+		};
 	public:
 		String();
 		String(const char *);
 		String(const String &);
 		String & operator =(const String &);
 		~String();
-		char & operator[](int);
+		CharProxy operator[](int);
+	//	char & operator[](int);
 		int size();
 		void print();
 		int use_count();
@@ -84,6 +127,10 @@ const char * String::c_str() const{
 	return _str;
 }
 
+String::CharProxy String::operator[](int idx){
+	return CharProxy(*this,idx);
+}
+#if 0
 char & String::operator [](int index){
 	if(index>=0 && index<size()){
 		if(_str[size() + 1]==1)
@@ -102,6 +149,7 @@ char & String::operator [](int index){
 		return nullchar;
 	}
 }
+#endif
 
 int main(){
 	String s1 = "how are you";
@@ -139,11 +187,7 @@ int main(){
 	s2.print();
 	cout<<"-------------------------------"<<endl;
 	cout<<"执行读操作: "<<endl;
-	cout<<s3[1]<<endl;
-	s3.print();	
-	printf("s3 address: %p\n",s3.c_str());
-	cout<<"s3's use_count: "<<s3.use_count()<<endl;
-	cout<<endl;
+	cout<<s2[0]<<endl;
 	cout<<"s1's use_count: "<<s1.use_count()<<endl;
 	cout<<"s2's use_count: "<<s2.use_count()<<endl;
 	printf("s1 address: %p\n",s1.c_str());
@@ -152,6 +196,11 @@ int main(){
 	s1.print();
 	cout<<"s2--> ";
 	s2.print();
+	cout<<endl;
+	s3.print();	
+	printf("s3 address: %p\n",s3.c_str());
+	cout<<"s3's use_count: "<<s3.use_count()<<endl;
+	cout<<endl;
 
 	return 0;
 }
